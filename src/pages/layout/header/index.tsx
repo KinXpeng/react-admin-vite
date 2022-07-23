e507b60e11e6e048d089483b2c0b3bb82e076b51
North <1227379879@qq.com>
2022-07-24 00:44:27 +0800

    fix: 修复主题色选中样式居中

--- src/pages/layout/header/index.tsx
+++ src/pages/layout/header/index.tsx
@@ -208,7 +208,7 @@ function HeaderNav({ width }: IHeaderProps) {
 						{colorList.map((item) => (
 							<Tooltip title={item[configStore.locale + '_name']} color={item.color + 'B3'} key={item.color}>
 								<span
-									className="w-5 h-5 leading-5 text-center rounded-sm text-white mr-2 cursor-pointer"
+									className="w-5 h-5 leading-5 text-center rounded-sm text-white mr-2 cursor-pointer flex items-center justify-center"
 									style={{ background: item.color }}
 									onClick={() => configStore.switchColor(item.color)}
 								>
