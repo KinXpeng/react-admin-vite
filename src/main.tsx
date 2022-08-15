4c860bb122da1bc0be6365d68bf7002cc73e4bef
KinXpeng <kinxpeng@163.com>
2022-08-15 17:11:30 +0800

    update:css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import '@/styles/index.css'
 import 'antd/dist/antd.variable.min.css'
 // 引入国际化
 import '@/i18n/index'
