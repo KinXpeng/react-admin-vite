48936e2efdcffd17321b149cdacca105231745cb
KinXpeng <52942521+KinXpeng@users.noreply.github.com>
2022-10-31 06:34:14 +0000

    Deploying to gh-pages from @ KinXpeng/react-admin-vite@f832c31e4ee7d09db8ee2b2193096a314a24b2a7 🚀

--- assets/index.3b1f21d9.js
+++
@@ -1,1 +0,0 @@
-import{G as r}from"./index.3bafaa84.js";function i(){return r("div",{children:r("p",{children:"DiaryRecords"})})}export{i as default};
---
+++ assets/index.7e05a5f5.js
@@ -0,0 +1,1 @@
+import{G as r}from"./index.9d01d6c1.js";function i(){return r("div",{children:r("p",{children:"DiaryRecords"})})}export{i as default};
