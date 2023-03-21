8e838b0d8bddf24d107ff9fd7fc4dea8f8fee5b1
KinXpeng <52942521+KinXpeng@users.noreply.github.com>
2023-03-21 07:54:02 +0000

    Deploying to gh-pages from @ KinXpeng/react-admin-vite@89c3ac3257128b7820b9ee10e804bd62e51fec11 🚀

--- assets/index.632e97a2.js
+++
@@ -1,1 +0,0 @@
-import{G as r}from"./index.af9ee330.js";function i(){return r("div",{children:r("p",{children:"DiaryRecords"})})}export{i as default};
--- assets/index.8958f76a.js
+++
@@ -1,1 +0,0 @@
-import{G as l}from"./index.af9ee330.js";const u=()=>l("div",{className:"w-full h-full",children:"\u5206\u6790\u9762\u677F"});export{u as default};
