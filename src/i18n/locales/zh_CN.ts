89c3ac3257128b7820b9ee10e804bd62e51fec11
KinXpeng <kinxpeng@163.com>
2023-03-21 15:52:53 +0800

    fix:1111

--- src/i18n/locales/zh_CN.ts
+++ src/i18n/locales/zh_CN.ts
@@ -1,37 +1,37 @@
 const zh_CN = {
-	notfound: '访问的页面不存在',
-	back_home: '返回首页',
-	home: '首页',
-	header: {
-		page_style: '整体风格设置',
-		theme_color: '主题色'
-	},
-	aside: {
-		panel: {
-			nav: '面板管理',
-			analysis_panel: '分析面板'
-		},
-		list: {
-			nav: '列表管理',
-			diary_list: '日记列表',
-			diary_records: '日记记录',
-			user_list: '用户列表'
-		},
-		personal: {
-			nav: '个人中心',
-			personal_status: '我的状态'
-		}
-	},
-	login: {
-		remember: '记住密码',
-		login: '登录',
-		username: '用户名：admin',
-		password: '密码：123456',
-		username_check: '请输入用户名',
-		password_check: '请输入密码',
-		success: '登录成功！',
-		error: '用户名或密码错误！'
-	}
+  notfound: '出错了，访问的页面不存在',
+  back_home: '返回首页',
+  home: '首页',
+  header: {
+    page_style: '整体风格设置',
+    theme_color: '主题色'
+  },
+  aside: {
+    panel: {
+      nav: '面板管理',
+      analysis_panel: '分析面板'
+    },
+    list: {
+      nav: '列表管理',
+      diary_list: '日记列表',
+      diary_records: '日记记录',
+      user_list: '用户列表'
+    },
+    personal: {
+      nav: '个人中心',
+      personal_status: '我的状态'
+    }
+  },
+  login: {
+    remember: '记住密码',
+    login: '登录',
+    username: '用户名：admin',
+    password: '密码：123456',
+    username_check: '请输入用户名',
+    password_check: '请输入密码',
+    success: '登录成功！',
+    error: '用户名或密码错误！'
+  }
 }
 
 export default zh_CN
